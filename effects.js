9efacae8eb969e66203c6333f2eeb710c6b17717
bkimz <writetobriankim@gmail.com>
2017-04-17 16:01:16 -0400

    first commit

---
+++ effects.js
@@ -0,0 +1,20 @@
+$("button").click(function(){
+
+	$("div").slideToggle();
+}
+
+/*
+$("button").click(function(){
+
+	$("div").fadeToggle();
+}
+
+$("button").click(function(){
+
+	$("div").slideUp("slow", function(){
+	$(this).remove();
+	});
+}
+
+
+*/
